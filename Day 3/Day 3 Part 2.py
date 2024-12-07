be2c206c88934fe0b236e022407616570d9c2619
Daniel <daniel.l.smith547@outlook.com>
2024-12-07 13:27:06 -0500

    Day 3 Part 2 Completed

--- Day 3/Day 3 Part 2.py
+++ Day 3/Day 3 Part 2.py
@@ -1,15 +1,33 @@
-file = open(r"/Day 3/Day 3 Example Text.txt", "r")
+file = open(r"C:\Users\danie\PycharmProjects\adventOfCode\Day 3\Day 3 Puzzle Input.txt", "r")
 
 characterString = file.read()
 maxIndex = len(characterString)
 mulSubstring = "mul("
 doSubstring = "do()"
 dontSubstring = "don't()"
+doSubstringIndexes = []
+dontSubstringIndexes = []
+
+# Get locations of Dos
+doIndex = -1
+while True:
+    doIndex = characterString.find(doSubstring, doIndex+1)
+    if doIndex == -1:
+        break
+    doSubstringIndexes.append(doIndex)
+
+# Get locations of Don'ts
+dontIndex = -1
+while True:
+    dontIndex = characterString.find(dontSubstring, dontIndex + 1)
+    if dontIndex == -1:
+        break
+    dontSubstringIndexes.append(dontIndex)
 
 lastIndexFound = -1
 currentSum = 0
 while True:
-    # get next instance of substring
+    # get next instance of mul substring
     currentIndex = characterString.find(mulSubstring, lastIndexFound+1)
     # if at end of file break out of loop
     if currentIndex == -1:
@@ -17,26 +35,21 @@ while True:
     lastIndexFound = currentIndex
 
     # Determine if do or don't
-    nearistDoIndex = -1
-    nearistDontIndex = -1
-    doIndex = characterString.find(doSubstring, nearistDoIndex+1)
-    dontIndex = characterString.find(dontSubstring, nearistDontIndex+1)
-    while doIndex < currentIndex or dontIndex < currentIndex and (nearistDoIndex != -1 and nearistDontIndex != -1):
-        doIndex = characterString.find(doSubstring, nearistDoIndex+1)
-        dontIndex = characterString.find(dontSubstring, nearistDontIndex+1)
-        if doIndex < currentIndex:
-
-            nearistDoIndex = doIndex
-        if dontIndex < currentIndex:
-            nearistDontIndex = dontIndex
-        if doIndex == -1 and dontIndex == -1:
+    doNum = 0
+    dontNum = -1
+    for num in doSubstringIndexes:
+        if num < currentIndex:
+            doNum = num
+        else:
             break
-
-    if nearistDontIndex > nearistDoIndex:
-        continue
-
-    if nearistDontIndex != -1 and nearistDoIndex == -1:
-        continue
+    for num in dontSubstringIndexes:
+        if num < currentIndex:
+            dontNum = num
+        else:
+            break
+    if dontNum != -1:
+        if doNum < dontNum:
+            continue
 
     # Get first number if valid
     firstNumber = ""
